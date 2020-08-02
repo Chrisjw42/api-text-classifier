3f2c843be875db7f681d1fdd265aed06c0370f02
Chrisjw42 <chrisjw42@gmail.com>
2020-08-02 15:05:24 +1000

    Feature/accept string (#2)
    * Modify text accepter
    
    * add pytest
    
    * clean yaml
    
    * test
    
    * try another
    
    * rq
    
    * rm rq
    
    * clean
    
    * rq
    
    * pytest
    
    * pytest
    
    * ls
    
    * switch to conda
    
    * conda
    
    * gcloud builder
    
    * fmt
    
    * miniconda builder
    
    * breakout
    
    * bug
    
    * alphine conda
    
    * pin rq
    
    * Update
    
    * isort
    
    * rq pinning
    
    * fix linting errors
    
    * md
    
    * reactivate build
    
    Co-authored-by: Chris <chris.williams@enea-consulting.com>

--- text_classifier/main.py
+++ text_classifier/main.py
@@ -1,28 +1,39 @@
-# https://fastapi.tiangolo.com/
+"""main API definition using FastApi
+"""
 from typing import Optional
 
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel # pylint: disable=E0611
 
 app = FastAPI()
 
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Optional[bool] = None
+# pylint: disable=too-few-public-methods
+class AssessmentRequest(BaseModel):
+    """Main request object, specifying the values that are required over the API.
+    """
+    # Each of these attributes can be passed in the request body
+    requester_name: Optional[str] = None
+    text: str
 
 
 @app.get("/")
 def read_root():
+    """Simple root returner for debugging
+
+    :return: hello world dict
+    :rtype: dict
+    """
     return {"Hello": "World"}
 
-# q is optional
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/assess_text")
+def assess_text(request: AssessmentRequest):
+    """Main assessment function, takes text and does the magic.
 
-# # Validates that item_id is an int 
-# @app.put("/items/{item_id}")
-# def update_item(item_id: int, item: Item):
-#     return {"item_name": item.name, "item_id": item_id}
+    :param request: main reqeust object, declaring this explicity gives editor support.
+    :type request: AssessmentRequest
+    :return: result of assessment
+    :rtype: dict
+    """
+
+    return {"passed_text": request.text, "morphed_text":request.text.upper()}
