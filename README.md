837f3d5323c5ab0d02235a4634690f9123e42ba4
Chrisjw42 <chrisjw42@gmail.com>
2020-08-02 12:40:53 +1000

    Feature/use gcloud runtime (#1)
    * Init
    
    * add cloudbuild yaml
    
    * update entrypoint
    
    * syntax
    
    * include app yaml
    
    * rm
    
    * add CD step
    
    * specify folder
    
    * set dir
    
    * instances
    
    * manual trigger
    
    * manual trigger
    
    * update runtime
    
    * Specify small instance class
    
    * reorganise
    
    * rq
    
    * rm put
    
    * deactivat instance class setting
    
    * use custom dockerfile
    
    * customer runtime
    
    * Update dockerfile dirs
    
    * force
    
    * force
    
    * restructure
    
    * init attempt
    
    * force
    
    Co-authored-by: Chris <chris.williams@enea-consulting.com>

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# api-text-classifier
+# fastapi-test
---
+++ text_classifier/main.py
@@ -0,0 +1,28 @@
+# https://fastapi.tiangolo.com/
+from typing import Optional
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Optional[bool] = None
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+# q is optional
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
+
+# # Validates that item_id is an int 
+# @app.put("/items/{item_id}")
+# def update_item(item_id: int, item: Item):
+#     return {"item_name": item.name, "item_id": item_id}
