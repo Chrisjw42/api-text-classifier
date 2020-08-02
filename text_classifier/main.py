a1e565a759c6e34ab46ccf2cdd9eb47cf303e3f7
Chris <chris.williams@enea-consulting.com>
2020-08-02 12:06:50 +1000

    force

--- text_classifier/main.py
+++ text_classifier/main.py
@@ -22,7 +22,7 @@ def read_root():
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
-# Validates that item_id is an int 
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+# # Validates that item_id is an int 
+# @app.put("/items/{item_id}")
+# def update_item(item_id: int, item: Item):
+#     return {"item_name": item.name, "item_id": item_id}
